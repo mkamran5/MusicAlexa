c8196e9303f82d1f38a348410d5b886253211b09
mkamran5 <116682851+mkamran5@users.noreply.github.com>
2022-10-29 09:46:26 +0500

    Hu

--- config/config.py
+++ config/config.py
@@ -51,7 +51,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TheTeamAlexa/AlexaMusic",
+    "https://github.com/mkamran5/MusicAlexa",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
